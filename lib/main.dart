07d01ad32a3f4f94213f8179ac5d375798895cbe
yousef ahmed <tourktousef@gmail.com>
2024-10-19 15:15:38 +0300

    spaces

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,6 @@ import 'package:flutter_advanced/core/routing/app_router.dart';
 import 'package:flutter_advanced/doc_app.dart';
 
 void main() {
-
-
-
-
-
-
-
-
   runApp(
     DocApp(
       appRouter: AppRouter(),
