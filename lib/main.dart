3a9916677a060eb17bd18cf815663a28e168529e
yousef ahmed <tourktousef@gmail.com>
2024-10-19 15:11:53 +0300

    -just fack

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter_advanced/core/routing/app_router.dart';
 import 'package:flutter_advanced/doc_app.dart';
 
 void main() {
-  
+
+
+
   runApp(
     DocApp(
       appRouter: AppRouter(),
